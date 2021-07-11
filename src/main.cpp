af524c7926348f9840c79641d81c1591d2bd7869
Lior Schwartz <lior@soltrex.io>
2021-07-11 18:07:54 +0300

    ready for testing in serial configuration

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,42 @@
 #include <Wire.h>
 #include "ros.h"
 
+#define DEBUG     true
+
 byte J12_buffer[4] = { 0 };
 byte J345_buffer[6] = { 0 };
+float Joints[5];
 
-void send_joint_positions(float Joints[5])
+void clear_serial_buffer(int iterations)
+{
+  for(int i=0;i<iterations;i++)
+  {
+    Serial.read();
+  }
+  return;
+}
+float getSerialfloat(String str)
+{
+  Serial.println(str);
+  clear_serial_buffer(100);
+  while(!Serial.available())
+  {
+    // nothing
+  }
+  float temp=Serial.parseFloat();
+  return temp;
+}
+void get_serial_joints(float val[0])
+{
+  Serial.println("enter joint values in radian:");
+  val[0] = getSerialfloat("J1:");
+  val[1]  = getSerialfloat("J2:");
+  val[2]  = getSerialfloat("J3:");
+  val[3]  = getSerialfloat("J4:");
+  val[4]  = getSerialfloat("J5:");
+}
+
+void send_joint_positions()
 {
   int16_t joint_buffer[5] = { 0 };
 
@@ -36,11 +68,28 @@ void send_joint_positions(float Joints[5])
   Wire.endTransmission();
 }
 
-void setup() {
-  // put your setup code here, to run once:
+
+// loop functions
+void serial_test_loop()
+{
+  get_serial_joints(&Joints[0]);
+  Serial.println("sending joint positions");
+  send_joint_positions();
+  Serial.println("positions sent\n");
+}
+
+
+void setup() 
+{
+  if(DEBUG)
+  {
+    Serial.begin(115200);
+  }
+
   Wire.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  serial_test_loop();
 }
