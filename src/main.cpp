c07983cfb7df67f7a005e5f853a09388d2dd151c
Soltrex-io <lior@soltrex.io>
2021-07-11 14:18:25 +0300

    added I2C send function and trying to integrate ROS

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "ros.h"
+
+byte J12_buffer[4] = { 0 };
+byte J345_buffer[6] = { 0 };
+
+void send_joint_positions(float Joints[5])
+{
+  int16_t joint_buffer[5] = { 0 };
+
+
+  for (int i=0; i<5; i++)
+  {
+    joint_buffer[i] = Joints[i] * 1000;
+  }
+
+  for(int i=0; i<2; i++)
+  {
+    J12_buffer[2*i] = joint_buffer[i] & 0xFF;
+    J12_buffer[2*i+1] = joint_buffer[i] >> 8;
+  }
+
+  for(int i=0; i<3; i++)
+  {
+    J345_buffer[2*i] = joint_buffer[i+2] & 0xFF;
+    J345_buffer[2*i+1] = joint_buffer[i+2] >> 8;
+  }
+
+  Wire.beginTransmission(0x02);
+  Wire.write(*J12_buffer);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x03);
+  Wire.write(*J345_buffer);
+  Wire.endTransmission();
+}
 
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();
 }
 
 void loop() {
